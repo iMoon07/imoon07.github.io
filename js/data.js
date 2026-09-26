d78f17fa43dd52c6bcd832de788e9b74beb997a2
Raja Muhammad Kurnia Setyawan <kurniatamvan13@gmail.com>
2026-09-26 17:51:12 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,43 +10,64 @@ const myProjects = [
     id: "linux-attack-chain",
     category: "experiments",
     publishedDate: "2026-09-02",
-    lastEditedDate: "2026-09-02",
+    lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/Linux-Attack-Chain-001/A-Journey-Through-a-Linux-Attack-Chain-(Lab-Practice)-id.md"
+  },
+  {
+    id: "linux-enumeration-investigation",
+    category: "experiments",
+    publishedDate: "2026-09-18",
+    lastEditedDate: "2026-09-26",
+    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/LINUX-ENUMERATION-INVESTIGATION/linux-enumeration-investigation-id.md"
+  },
+  {
+    id: "reverse-shell-detection",
+    category: "experiments",
+    publishedDate: "2026-09-19",
+    lastEditedDate: "2026-09-26",
+    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/REVERSE-SHELL-DETECTION/reverse-shell-server-side.md"
   },
   {
     id: "command-injection-reverse-shell",
     category: "experiments",
-    publishedDate: "2026-09-26",
+    publishedDate: "2026-09-20",
     lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/COMMAND-INJECTION-REVERSE-SHELL/command-injection-reverse-shell-id.md"
   },
   {
     id: "post-exploitation-enumeration",
     category: "experiments",
-    publishedDate: "2026-09-26",
+    publishedDate: "2026-09-21",
     lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-ENUMERATION/post-exploitation-enum-id.md"
   },
   {
     id: "post-exploitation-file-system-explore",
     category: "experiments",
-    publishedDate: "2026-09-26",
+    publishedDate: "2026-09-22",
     lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-FILE-SYSTEM-EXPLORE/post-exploitation-filesystemexplore-id.md"
   },
   {
     id: "post-exploitation-foothold",
     category: "experiments",
-    publishedDate: "2026-09-26",
+    publishedDate: "2026-09-23",
     lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-FOOTHOLD/post-exploitation-foothold-id.md"
   },
   {
-    id: "post-exploitation-lateral-movement",
+    id: "post-exploitation-privilege-enumeration",
     category: "experiments",
-    publishedDate: "2026-09-26",
+    publishedDate: "2026-09-24",
     lastEditedDate: "2026-09-26",
-    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-LATERAL-MOVEMENT/post-exploitation-lateral-movement-id.md"
+    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-PRIVILEGE-ENUMERATION/post-exploitation-priv-enum-id.md"
+  },
+  {
+    id: "post-exploitation-privilege-escalation",
+    category: "experiments",
+    publishedDate: "2026-09-25",
+    lastEditedDate: "2026-09-26",
+    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-PRIVILEGE-ESCALATION/post-exploitation-priv-esca-copyfail-id.md"
   },
   {
     id: "post-exploitation-persistent",
@@ -63,18 +84,11 @@ const myProjects = [
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-PIVOTING/post-exploitation-pivoting-id.md"
   },
   {
-    id: "post-exploitation-privilege-enumeration",
+    id: "post-exploitation-lateral-movement",
     category: "experiments",
     publishedDate: "2026-09-26",
     lastEditedDate: "2026-09-26",
-    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-PRIVILEGE-ENUMERATION/post-exploitation-priv-enum-id.md"
-  },
-  {
-    id: "post-exploitation-privilege-escalation",
-    category: "experiments",
-    publishedDate: "2026-09-26",
-    lastEditedDate: "2026-09-26",
-    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-PRIVILEGE-ESCALATION/post-exploitation-priv-esca-copyfail-id.md"
+    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/POST-EXPLOITATION-LATERAL-MOVEMENT/post-exploitation-lateral-movement-id.md"
   },
   {
     id: "sqli-webshell-revshell",
@@ -89,20 +103,6 @@ const myProjects = [
     publishedDate: "2026-09-26",
     lastEditedDate: "2026-09-26",
     rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/WEBSHELL-REVERSE-SHELL/webshell-reverse-shell-id.md"
-  },
-  {
-    id: "linux-enumeration-investigation",
-    category: "experiments",
-    publishedDate: "2026-09-26",
-    lastEditedDate: "2026-09-26",
-    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/LINUX-ENUMERATION-INVESTIGATION/linux-enumeration-investigation-id.md"
-  },
-  {
-    id: "reverse-shell-detection",
-    category: "experiments",
-    publishedDate: "2026-09-26",
-    lastEditedDate: "2026-09-26",
-    rawUrl: "https://raw.githubusercontent.com/iMoon07/Penjelajah-CyberSecurity/main/Experiments/REVERSE-SHELL-DETECTION/reverse-shell-server-side.md"
   }
 
   // Template for adding a new article manually:
